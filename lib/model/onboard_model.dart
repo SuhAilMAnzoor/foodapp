d89895e71667f2258b079d73b5e869c447b3b884
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-11-28 03:25:32 +0500

    OnBoard Model is added

---
+++ lib/model/onboard_model.dart
@@ -0,0 +1,28 @@
+class OnBoard {
+  final String image, text1, text2;
+
+  OnBoard({
+    required this.image,
+    required this.text1,
+    required this.text2
+  });
+}
+
+List<OnBoard> onboards = [
+
+  OnBoard(
+      image: 'assets/images/onboard_1.png',
+      text1: 'Grap your \nDelicious food !',
+      text2: 'Delivery food in your phone'
+  ),
+  OnBoard(
+      image: 'assets/images/onboard_2.png',
+      text1: 'Grap your \nDelicious food !',
+      text2: 'Delivery food in your phone'
+  ),
+  OnBoard(
+      image: 'assets/images/onboard_3.png',
+      text1: 'Grap your \nDelicious food !',
+      text2: 'Delivery food in your phone'
+  ),
+];
