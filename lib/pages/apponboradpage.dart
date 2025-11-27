d5410a2601732a37519782330cfdb8697cf2a469
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-11-28 01:56:11 +0500

    On Board Page is First Page Of App

---
+++ lib/pages/apponboradpage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class AppOnBoardPage extends StatefulWidget {
+  const AppOnBoardPage({super.key});
+
+  @override
+  State<AppOnBoardPage> createState() => _AppOnBoardPageState();
+}
+
+class _AppOnBoardPageState extends State<AppOnBoardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kblack,
+    );
+  }
+}
